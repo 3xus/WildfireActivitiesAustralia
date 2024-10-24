11ff673d04f87eccbf954589d584b97f088b56b2
3xus <primeexus@gmail.com>
2024-10-24 13:12:33 -0600

    Fin de las tareas de la primera parte

--- WildfireActivitiesAustralia.py
+++ WildfireActivitiesAustralia.py
@@ -57,3 +57,80 @@ plt.show()
 # Task 1.3. Let's have an insight on the distribution of mean estimated fire brightness 
 # across the regions use the functionality of seaborn to develop a barplot
 df['Region'].unique()
+
+# Creating a bar plot using seaborn to visualize the distribution of mean estimated fire 
+# brightness across regions
+plt.figure(figsize=(10,6))
+
+# Using seaborn's barplot function to create the plot
+sns.barplot(data = df, x='Region', y='Mean_estimated_fire_brightness')
+plt.xlabel('Region')
+plt.ylabel('Mean Estimated Fire Brightness (Kelvin)')
+plt.title('Distribution of Mean Estimated Fire Brightness across Region')
+plt.show()
+
+# Task 1.4: Let's find the portion of count of pixels for presumed vegetation fires 
+# vary across regions we will develop a pie chart for this
+region_count = df.groupby('Region')['Count'].sum()
+
+# Creating a pie chart to visualize the portion of count of pixels for presumed 
+# vegetation fires across regions
+plt.figure(figsize=(10,6))
+plt.pie(region_counts, labels = region_count.index)
+plt.title('Percentage of Pixels for Presumed Vegetation Fires by Region')
+plt.legend([(i,round(k/region_counts.sum()*100,2)) for i,k in zip(region_counts.index, region_counts)])
+plt.axis('equal')
+plt.show()
+
+# Task 1.6: Let's try to develop a histogram of the mean estimated fire 
+# brightness. Using Matplotlib to create the histogram
+plt.figure(figsize=(10,6))
+plt.hist(x=df['Mean_estimated_fire_brightness'], bins=20)
+plt.xlabel('Mean Estimated Fire Brightness (Kelvin)')
+plt.ylabel('Count')
+plt.title('Histogram of Mean Estimated Fire Brightness')
+plt.show()
+
+# Creating a histogram to visualize the distribution of mean estimated fire brightness across regions using Seaborn
+# Using sns.histplot to create the histogram
+# Specifying the DataFrame (data=df) and the column for the x-axis (x='Mean_estimated_fire_brightness')
+# Adding hue='Region' to differentiate the distribution across regions
+sns.histplot(data=df, x='Mean_estimated_fire_brightness', hue='Region', multiple='stack')
+plt.show()
+
+# Task 1.8. Let's try to find of there is any correlation between mean estimated fire
+# radiative power and mean confidence level
+plt.figure(figsize=(8,6))
+sns.scatterplot(data=df, x='Mean_confidence', y='Mean_estimated_fire_radiative_power')
+plt.xlabel('Mean Estimated Fire Radiative Power (MW)')
+plt.ylabel('Mean Confidence')
+plt.title('Mean Estimated Fire Radiative Power vs. Mean Confidence')
+plt.show()
+
+## Task 1.9. Let's mark these seven regions on the Map of Australia Usinf Folium
+region_data = {'region':['NSW','QL','SA','TA','VI','WA','NT'], 'Lat':[-31.8759835,-22.1646782,-30.5343665,-42.035067,-36.5986096,-25.2303005,-19.491411], 
+               'Lon':[147.2869493,144.5844903,135.6301212,146.6366887,144.6780052,121.0187246,132.550964]}
+reg=pd.DataFrame(region_data)
+## reg
+
+# Instantiate a feature group
+aus_reg = folium.map.FeatureGroup()
+
+# Map centered on Australia
+Aus_map = folium.Map(location = [-25,135], zoom_start=4)
+
+# Loop through the region and add to feature group
+for lat, lng, lab in zip(reg.Lat, reg.Lon, reg.region):
+    aus_reg.add_child(
+        folium.features.CircleMarker(
+            [lat,lng],
+            popup = lab,
+            radius = 5,
+            color = 'red',
+            fill = True,
+            fill_color = 'blue',
+            fill_opacity=0.6            
+        )
+    )
+    
+Aus_map.add_child(aus_reg)
